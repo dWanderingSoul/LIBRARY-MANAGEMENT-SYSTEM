0e449cc7e3dc719b889df22fb602985d1d4f96e1
d Wandering_Soul <teeymseey@gmail.com>
2024-06-07 19:15:12 +0100

    update

---
+++ user.js
@@ -0,0 +1,12 @@
+// ES6 class user
+export class User {
+    constructor(name, id) {
+      this.name = name;
+      this.id = id;
+      this.borrowedBooks = [];// Array to store borrowed books
+    }
+  
+  }
+ 
+  
+  
